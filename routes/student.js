072760e3d80f30e0042888c8ae9592620e73df1a
sneh <bhikadiyasneh68@gmail.com>
2022-12-24 17:00:34 +0530

    Revert "joi-pratice"
    This reverts commit 5e2175c3fe330053072b6df77b56ac9e08e153a7.

--- routes/student.js
+++ routes/student.js
@@ -3,10 +3,10 @@ const route=express.Router();
 const studentController=require('../controller/studentController');
 const validater=require('../utils/requestvalidation');
 
-// route.get('/',studentController.getstudet);
-// route.get('/:id',studentController.getOnestudet);
-// route.post('/',validater('student'),studentController.createStudent);
-// route.put('/:id',studentController.updateStudent);
-// route.delete('/:id',studentController.deleteStudent);
+route.get('/',studentController.getstudet);
+route.get('/:id',studentController.getOnestudet);
+route.post('/',validater('student'),studentController.createStudent);
+route.put('/:id',studentController.updateStudent);
+route.delete('/:id',studentController.deleteStudent);
 
 module.exports=route;
